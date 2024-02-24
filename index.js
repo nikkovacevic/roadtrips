e7f7888e4be0c8311bf0997d56995de297fce0ff
Nik Kovačević <nik.kovacevic@student.um.si>
2024-02-24 22:33:20 +0100

    Auth done

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const spotifyRoutes = require('./routes/spotifyAuth');
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
-app.use(
-	session({
-		secret: process.env.SESSION_SECRET,
-		resave: false,
-		saveUninitialized: true
-	})
-);
 
 app.use('/spotify', spotifyRoutes);
 
